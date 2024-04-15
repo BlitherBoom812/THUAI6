c0d23e71ec1df05cf051c856d478f8ec2d02c22e
eesast <eesast@tsinghua.edu.cn>
2024-04-15 16:36:44 +0800

    dependency of run.sh

--- dependency/shell/run.sh
+++ dependency/shell/run.sh
@@ -1,71 +1,83 @@
 #!/usr/local
-
 python_dir=/usr/local/PlayerCode/CAPI/python/PyAPI
 python_main_dir=/usr/local/PlayerCode/CAPI/python
 playback_dir=/usr/local/playback
+map_dir=/usr/local/map
 
-if [ $EXPOSED -eq 1 ]; then
-    nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $TIME --mode $MODE --mapResource $MAP --url $URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
-    server_pid=$!
-else
-    nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $TIME --mode $MODE --mapResource $MAP --notAllowSpectator --url $URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
-    server_pid=$!
-fi
-sleep 5
-for k in {1..2}
-do
-    pushd /usr/local/team$k
-    if [ $k -eq 1 ]; then
-        for i in {1..4}
-        do
-            j=$((i - 1))
-            if [ -f "./player$i.py" ]; then
-                cp -r $python_main_dir $python_main_dir$i
-                cp -f ./player$i.py $python_main_dir$i/PyAPI/AI.py
-                nice -0 python3 $python_main_dir$i/PyAPI/main.py -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
-            elif [ -f "./capi$i" ]; then
-                nice -0 ./capi$i -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
-            else
-                echo "ERROR. $i is not found."
-            fi
-        done
+if [ "$TERMINAL" = "SERVER" ]; then
+    map_path=$map_dir/$MAP_ID.txt
+    if [ $EXPOSED -eq 1 ]; then
+        nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $TIME --mode $MODE --mapResource $map_path --url $URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
+        server_pid=$!
     else
-        for i in {5..5}
-        do
-            j=$((i - 1))
-            if [ -f "./player$i.py" ]; then
-                cp -r $python_main_dir $python_main_dir$i
-                cp -f ./player$i.py $python_main_dir$i/PyAPI/AI.py
-                nice -0 python3 $python_main_dir$i/PyAPI/main.py -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
-            elif [ -f "./capi$i" ]; then
-                nice -0 ./capi$i -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
-            else
-                echo "ERROR. $i is not found."
-            fi
-        done
+        nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $TIME --mode $MODE --mapResource $map_path --notAllowSpectator --url $URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
+        server_pid=$!
     fi
+
+    sleep 10
+
+    if [ -f $playback_dir/start.lock ]; then
+        ps -p $server_pid
+        while [ $? -eq 0 ]
+        do
+            sleep 1
+            ps -p $server_pid > /dev/null 2>&1
+        done
+        # result=$(cat /usr/local/playback/result.json)
+        # score0=$(echo "$result" | grep -oP '(?<="Student":)\d+')
+        # score1=$(echo "$result" | grep -oP '(?<="Tricker":)\d+')
+        # curl $URL -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"result":[{"team_id":0, "score":'${score0}'}, {"team_id":1, "score":'${score1}'}], "mode":'${MODE}'}'> $playback_dir/send.log 2>&1
+        touch $playback_dir/finish.lock
+        echo "Finish"
+    else
+        echo "Failed to start game."
+        touch $playback_dir/finish.lock
+        touch temp.lock
+        mv -f temp.lock $playback_dir/video.thuaipb
+        kill -9 $server_pid
+    fi
+
+elif [ "$TERMINAL" = "CLIENT" ]; then
+    echo "Client Mode! Team Label data - $TEAM_LABEL"
+    
+    # parse team label name
+    # k is an enum (1,2), 1 = STUDENT, 2 = TRICKER
+    if [ "$TEAM_LABEL" = "Student" ]; then
+        k=1
+    elif [ "$TEAM_LABEL" = "Tricker" ]; then
+        k=2
+    fi
+    pushd /usr/local/code
+        if [ $k -eq 1 ]; then
+            for i in {1..4}
+            do
+                j=$((i - 1)) # student id from 0 to 3
+                code_name=Student$i
+                if [ -f "./$code_name.py" ]; then
+                    cp -r $python_main_dir $python_main_dir$i
+                    cp -f ./$code_name.py $python_main_dir$i/PyAPI/AI.py
+                    nice -0 python3 $python_main_dir$i/PyAPI/main.py -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+                elif [ -f "./$code_name" ]; then
+                    nice -0 ./$code_name -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+                else
+                    echo "ERROR. $code_name is not found."
+                fi
+            done
+        else
+            i=5
+            j=4 # tricker id is 4
+            code_name=Tricker
+            if [ -f "./$code_name.py" ]; then
+                cp -r $python_main_dir $python_main_dir$i
+                cp -f ./$code_name.py $python_main_dir$i/PyAPI/AI.py
+                nice -0 python3 $python_main_dir$i/PyAPI/main.py -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+            elif [ -f "./$code_name" ]; then
+                nice -0 ./$code_name -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+            else
+                echo "ERROR. $code_name is not found."
+            fi
+        fi
     popd
-done
-
-sleep 10
-
-if [ -f $playback_dir/start.lock ]; then
-    ps -p $server_pid
-    while [ $? -eq 0 ]
-    do
-        sleep 1
-        ps -p $server_pid > /dev/null 2>&1
-    done
-    # result=$(cat /usr/local/playback/result.json)
-    # score0=$(echo "$result" | grep -oP '(?<="Student":)\d+')
-    # score1=$(echo "$result" | grep -oP '(?<="Tricker":)\d+')
-    # curl $URL -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"result":[{"team_id":0, "score":'${score0}'}, {"team_id":1, "score":'${score1}'}], "mode":'${MODE}'}'> $playback_dir/send.log 2>&1
-    touch $playback_dir/finish.lock
-    echo "Finish"
 else
-    echo "Failed to start game."
-    touch $playback_dir/finish.lock
-    touch temp.lock
-    mv -f temp.lock $playback_dir/video.thuaipb
-    kill -9 $server_pid
+    echo "VALUE ERROR: TERMINAL is neither SERVER nor CLIENT."
 fi
