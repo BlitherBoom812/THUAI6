c374e94bc48f64c7cf92f3b484ad7e4e88f0c200
Guoyun Tian <guoyun812@qq.com>
2024-04-23 10:31:25 +0800

    fix(bugs): fix some bugs of run.sh

--- dependency/shell/run.sh
+++ dependency/shell/run.sh
@@ -1,10 +1,15 @@
 #!/usr/local
 python_dir=/usr/local/PlayerCode/CAPI/python/PyAPI
 python_main_dir=/usr/local/PlayerCode/CAPI/python
-playback_dir=/usr/local/playback
+playback_dir=/usr/local/output
 map_dir=/usr/local/map
+mkdir -p $playback_dir
 
-TEAM_SEQ_ID=-1 # default value
+TEAM_SEQ_ID=0 # default value
+EXPOSED=0
+SCORE_URL=www.baidu.com
+MODE=0
+GAME_TIME=100
 
 get_current_team_label() {
     if [ $TEAM_SEQ_ID -eq $2 ]; then
@@ -38,14 +43,17 @@ read_array() {
 if [ "$TERMINAL" = "SERVER" ]; then
     map_path=$map_dir/$MAP_ID.txt
     if [ $EXPOSED -eq 1 ]; then
-        nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $TIME --mode $MODE --mapResource $map_path --url $URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
+        nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $GAME_TIME --mode $MODE --mapResource $map_path --url $SCORE_URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
         server_pid=$!
     else
-        nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $TIME --mode $MODE --mapResource $map_path --notAllowSpectator --url $URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
+        echo "Run NO EXPOSED"
+        nice -10 ./Server --port 8888 --studentCount 4 --trickerCount 1 --resultFileName $playback_dir/result --gameTimeInSecond $GAME_TIME --mode $MODE --mapResource $map_path --notAllowSpectator --url $SCORE_URL --token $TOKEN --fileName $playback_dir/video --startLockFile $playback_dir/start.lock > $playback_dir/server.log 2>&1 &
         server_pid=$!
+        echo "server pid: $server_pid"
+        ls $playback_dir
     fi
 
-    sleep 10
+    sleep 100
 
     if [ -f $playback_dir/start.lock ]; then
         ps -p $server_pid
@@ -72,7 +80,7 @@ elif [ "$TERMINAL" = "CLIENT" ]; then
     echo "Client Mode! Team Label data - $TEAM_LABEL"
     
     # parse team label name
-    $current_team_label=$TEAM_LABEL
+    current_team_label=$TEAM_LABEL
 
     # k is an enum (1,2), 1 = STUDENT, 2 = TRICKER
     if [ "$current_team_label" = "Student" ]; then
@@ -90,11 +98,15 @@ elif [ "$TERMINAL" = "CLIENT" ]; then
                 j=$((i - 1)) # student player id from 0 to 3
                 code_name=Student$i
                 if [ -f "./$code_name.py" ]; then
+                    echo "find ./$code_name.py"
                     cp -r $python_main_dir $python_main_dir$i
                     cp -f ./$code_name.py $python_main_dir$i/PyAPI/AI.py
                     nice -0 python3 $python_main_dir$i/PyAPI/main.py -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+                    ps -aux |grep main.py
                 elif [ -f "./$code_name" ]; then
+                    echo "find ./$code_name"
                     nice -0 ./$code_name -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+                    ps -aux |grep $code_name
                 else
                     echo "ERROR. $code_name is not found."
                 fi
@@ -104,15 +116,20 @@ elif [ "$TERMINAL" = "CLIENT" ]; then
             j=4 # tricker id is 4
             code_name=Tricker
             if [ -f "./$code_name.py" ]; then
+                echo "find ./$code_name.py"
                 cp -r $python_main_dir $python_main_dir$i
                 cp -f ./$code_name.py $python_main_dir$i/PyAPI/AI.py
                 nice -0 python3 $python_main_dir$i/PyAPI/main.py -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+                ps -aux |grep main.py
             elif [ -f "./$code_name" ]; then
+                echo "find ./$code_name"
                 nice -0 ./$code_name -I 127.0.0.1 -P 8888 -p $j > $playback_dir/team$k-player$j.log 2>&1 &
+                ps -aux |grep $code_name
             else
                 echo "ERROR. $code_name is not found."
             fi
         fi
+        sleep $((GAME_TIME * 2))
     popd
 else
     echo "VALUE ERROR: TERMINAL is neither SERVER nor CLIENT."
